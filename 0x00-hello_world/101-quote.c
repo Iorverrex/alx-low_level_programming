445e715694a90d335e801b5dcd9d6e9ef3204b1e
Iorverrex <iorvertersur@gmail.com>
2023-06-16 15:17:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -printing without printf and put
+ * main - printing without printf and put
  *
- * Return: is always (1)
+ * Return: always (1)
  *
  */
-init main(void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
-}
+};
