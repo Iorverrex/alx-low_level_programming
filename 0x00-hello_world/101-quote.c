f6b3cb3adf10a04b1412ce40b324a39893e34f16
Iorverrex <iorvertersur@gmail.com>
2023-06-16 15:31:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - printing without printf and put
+ * main - printing without printf and puts
  *
  * Return: always (1)
  *
@@ -9,6 +9,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
-};
+}
