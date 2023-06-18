2a3f3046bad48b34ba198c7aa834d4840c925e82
Iorverrex <iorvertersur@gmail.com>
2023-06-17 23:24:04 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -numbers of base 16 with lowercase
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 102)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
