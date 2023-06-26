30945687792509805c7d5b54776418d1afa68bbe
Iorverrex <iorvertersur@gmail.com>
2023-06-26 15:43:55 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,28 +4,23 @@
  *
  * @size: integer to set square size
  */
-int main(void)
+void print_square(int size)
 {
-	void print_square(int size)
+	int i;
+	int j;
+
+	if (size <= 0)
 	{
-		size = 10;
-		int i;
-		int j;
-
-		if (size <= 0)
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < size; i++)
 		{
-			putchar("\n");
-		} else
-		{
-			for (i = 0; i < size; i++)
+			for (j = 0; j < size; j++)
 			{
-				for (j = 0; j < size; j++)
-				{
-					_putchar('#');
-				};
-			};
+				_putchar('#');
+			}
 			_putchar('\n');
-		};
-	};
+		}
+	}
 }
-
